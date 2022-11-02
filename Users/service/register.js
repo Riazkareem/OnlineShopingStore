7cf30942cbd0df40d908d2bac5c10bb6bc45462c
Riazkareem <riazahmadgithub@gmail.com>
2022-11-02 12:49:16 +0500

    user module updated

---
+++ Users/service/register.js
@@ -0,0 +1,92 @@
+const AWS = require("aws-sdk");
+AWS.config.update({
+  region: "us-east-1",
+});
+const util = require("../utils/util");
+const bcrypt = require("bcryptjs");
+const uuid = require("uuid");
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+//const userTable = 'jinmeister-users';
+const userTable = process.env.USERS_TABLE;
+
+async function register(userInfo) {
+  const name = userInfo.name;
+  const email = userInfo.email;
+  const username = userInfo.username;
+  const password = userInfo.password;
+  if (!username || !name || !email || !password) {
+    return util.buildResponse(401, {
+      message: "All fields are required",
+    });
+  }
+
+  const dynamoUser = await getUser(email);
+  if (dynamoUser && dynamoUser.email) {
+    return util.buildResponse(401, {
+      message:
+        "user Email already exists in our database. please choose a different user Email",
+    });
+  }
+
+  const encryptedPW = bcrypt.hashSync(password.trim(), 10);
+  const user = {
+    name: name,
+    email: email,
+    id: uuid.v1(),
+    createdAt: new Date().toISOString(),
+    username: username.toLowerCase().trim(),
+    password: encryptedPW,
+  };
+
+  const saveUserResponse = await saveUser(user);
+  if (!saveUserResponse) {
+    return util.buildResponse(503, {
+      message: "Server Error. Please try again later.",
+    });
+  }
+
+  return util.buildResponse(200, { user });
+}
+
+async function getUser(email) {
+  const params = {
+    TableName: userTable,
+    Key: {
+      email: email,
+    },
+  };
+
+  return await dynamodb
+    .get(params)
+    .promise()
+    .then(
+      (response) => {
+        return response.Item;
+      },
+      (error) => {
+        console.error("There is an error getting user: ", error);
+      }
+    );
+}
+
+async function saveUser(user) {
+  const params = {
+    TableName: userTable,
+    Item: user,
+  };
+
+  return await dynamodb
+    .put(params)
+    .promise()
+    .then(
+      () => {
+        return true;
+      },
+      (error) => {
+        console.error("There is an error saving user: ", error);
+      }
+    );
+}
+
+module.exports.register = register;
--- Users/users.js
+++ Users/users.js
@@ -1,14 +1,14 @@
-'use strict';
-const AWS = require('aws-sdk');
+"use strict";
+const AWS = require("aws-sdk");
 const db = new AWS.DynamoDB.DocumentClient();
-const uuid = require('uuid');
+const uuid = require("uuid");
 
-const userTable = process.env.USER_TABLE;
+const userTable = process.env.USERS_TABLE;
 // Create a response
 function response(statusCode, message) {
   return {
     statusCode: statusCode,
-    body: JSON.stringify(message)
+    body: JSON.stringify(message),
   };
 }
 function sortByDate(a, b) {
@@ -16,128 +16,122 @@ function sortByDate(a, b) {
     return -1;
   } else return 1;
 }
-// Create a user
-module.exports.createUser = (event, context, callback) => {
-  // const post = {
-  //   id: uuid.v1 (),
-  //   createdAt: new Date().toISOString(),
-  //   body: event.body
-  // };
-  
-  // return db
-  //   .put({
-  //     TableName: userTable,
-  //     Item: post
-  //   })
-  //   .promise()
-  //   .then(() => {
-  //     callback(null, response(201, post));
-  //   })
-  //   .catch((err) => response(null, response(err.statusCode, err)));
-
-    const  body=JSON.parse(event.body);
-    return db
-      .put({
-        TableName: userTable,
-        Item:body
-      })
-      .promise()
-      .then(() => {
-        callback(null, response(201, body));
-      })
-      .catch((err) => response(null, response(err.statusCode, err)));
-};
-
 // Get all users
 module.exports.getAllUsers = (event, context, callback) => {
-    return db
-      .scan({
-        TableName: userTable
-      })
-      .promise()
-      .then((res) => {
-        callback(null, response(200, res.Items.sort(sortByDate)));
-      })
-      .catch((err) => callback(null, response(err.statusCode, err)));
-  };
-  // Get number of user
-  module.exports.getUsers = (event, context, callback) => {
-    const numberOfPosts = event.pathParameters.number;
-    const params = {
+  return db
+    .scan({
       TableName: userTable,
-      Limit: numberOfPosts
-    };
-    return db
-      .scan(params)
-      .promise()
-      .then((res) => {
-        callback(null, response(200, res.Items.sort(sortByDate)));
-      })
-      .catch((err) => callback(null, response(err.statusCode, err)));
+    })
+    .promise()
+    .then((res) => {
+      callback(null, response(200, res.Items.sort(sortByDate)));
+    })
+    .catch((err) => callback(null, response(err.statusCode, err)));
+};
+// Get number of user
+module.exports.getUsers = (event, context, callback) => {
+  const numberOfPosts = event.pathParameters.number;
+  const params = {
+    TableName: userTable,
+    Limit: numberOfPosts,
   };
-  // Get a single users
-  module.exports.getUser = (event, context, callback) => {
-    const id = event.pathParameters.id;
-  
-    const params = {
-      Key: {
-        id: id
-      },
-      TableName: userTable
-    };
-  
-    return db
-      .get(params)
-      .promise()
-      .then((res) => {
-        if (res.Item) callback(null, response(200, res.Item));
-        else callback(null, response(404, { error: 'User not found' }));
-      })
-      .catch((err) => callback(null, response(err.statusCode, err)));
-  };
-  // Update a user
+  return db
+    .scan(params)
+    .promise()
+    .then((res) => {
+      callback(null, response(200, res.Items.sort(sortByDate)));
+    })
+    .catch((err) => callback(null, response(err.statusCode, err)));
+};
+// Get a single users
+module.exports.getUser = (event, context, callback) => {
+  const email = event.pathParameters.email;
 
-  module.exports.updateUser = (event, context, callback) => {
-    const id = event.pathParameters.id;
-    const params = {
-      Key: {
-        id: id
-      },
-      TableName: userTable,
-      ConditionExpression: 'attribute_exists(id)',
-      UpdateExpression: 'SET body = :body',
-      ExpressionAttributeValues: {
-        ':body': event.body
-      },
-      ReturnValues: 'ALL_NEW'
-    };
-    console.log('Updating');
-  
-    return db
-      .update(params)
-      .promise()
-      .then((res) => {
-        console.log(res);
-        callback(null, response(200, { message: 'User Updated successfully' }));
-        // callback(null, response(200, res.Attributes));
-      })
-      .catch((err) => callback(null, response(err.statusCode, err)));
+  const params = {
+    Key: {
+      email: email,
+    },
+    TableName: userTable,
   };
 
-  // Delete a user
-  module.exports.deleteUser = (event, context, callback) => {
-    const id = event.pathParameters.id;
-    const params = {
-      Key: {
-        id: id
-      },
-      TableName: userTable
-    };
-    return db
-      .delete(params)
-      .promise()
-      .then(() =>
-        callback(null, response(200, { message: 'User deleted successfully' }))
-      )
-      .catch((err) => callback(null, response(err.statusCode, err)));
+  return db
+    .get(params)
+    .promise()
+    .then((res) => {
+      if (res.Item) callback(null, response(200, res.Item));
+      else callback(null, response(404, { error: "User not found" }));
+    })
+    .catch((err) => callback(null, response(err.statusCode, err)));
+};
+// Update a user
+module.exports.updateUser = (
+  event,
+  updateKey,
+  updateValue,
+  context,
+  callback
+) => {
+  const requestBody = JSON.parse(event.body);
+  return modifyProduct(
+    requestBody.email,
+    requestBody.updateKey,
+    requestBody.updateValue
+  );
+};
+async function modifyProduct(email, updateKey, updateValue) {
+  const params = {
+    TableName: userTable,
+    Key: {
+      email: email,
+    },
+    UpdateExpression: `set ${updateKey} = :value`,
+    ExpressionAttributeValues: {
+      ":value": updateValue,
+    },
+    ReturnValues: "UPDATED_NEW",
   };
+  return await db
+    .update(params)
+    .promise()
+    .then(
+      (response) => {
+        const body = {
+          Operation: "UPDATE",
+          Message: "SUCCESS",
+          UpdatedAttributes: response,
+        };
+        return buildResponse(200, body);
+      },
+      (error) => {
+        console.error("Query Failed: ", error);
+      }
+    );
+}
+//only Responce for update operation
+function buildResponse(statusCode, body) {
+  return {
+    statusCode: statusCode,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  };
+}
+
+// Delete a user
+module.exports.deleteUser = (event, context, callback) => {
+  const email = event.pathParameters.email;
+  const params = {
+    Key: {
+      email: email,
+    },
+    TableName: userTable,
+  };
+  return db
+    .delete(params)
+    .promise()
+    .then(() =>
+      callback(null, response(200, { message: "User deleted successfully" }))
+    )
+    .catch((err) => callback(null, response(err.statusCode, err)));
+};
